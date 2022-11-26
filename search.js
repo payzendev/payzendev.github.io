025bbe20e677432088124f21d7535f506c0f92a3
payzendev <102274938+payzendev@users.noreply.github.com>
2022-11-26 16:45:55 -0600

    Delete search.js

--- search.js
+++
@@ -1,5 +0,0 @@
-
-var div = document.createElement('div');
-div.innerHTML = "Search for: \""+localStorage.getItem("searchQuery")+"\"";
-div.setAttribute('class', 'searchFor'); 
-document.body.appendChild(div);
